7bba69706304ec98660192a575069651e69ea7cb
zijian-z <zijian-zhang@outlook.com>
2021-01-22 14:40:41 +0800

    添加用户头像

--- src/page/navbar.js
+++ src/page/navbar.js
@@ -1,20 +1,30 @@
 import React, {useEffect, useState} from "react";
-import {ButtonGroup, Button} from "@chakra-ui/react";
+import {ButtonGroup, Button, Avatar} from "@chakra-ui/react";
 import instance from "../config/axiosConfig";
 import {useHistory} from "react-router-dom";
+import {least15String} from "../util/stringUtil";
 
 export default function Navbar() {
     const [username, setUsername] = useState("");
+    const [imageData, setImageData] = useState("");
     const history = useHistory();
     const style = {
-        marginBottom: "20px",
-        marginTop: "5px"
+        group: {
+            marginBottom: "20px",
+            marginTop: "5px"
+        },
+        avatar: {
+            cursor: 'pointer'
+        }
+
     }
+
     useEffect(() => {
         async function status() {
             try {
                 const response = await instance.get('/users/status');
                 setUsername(response.data);
+                setImageData(least15String(response.data))
             } catch (error) {
                 console.log(error);
             }
@@ -36,7 +46,7 @@ export default function Navbar() {
     function Welcome() {
         if (username === "") {
             return(
-                <ButtonGroup style={style}>
+                <ButtonGroup style={style.group}>
                     <Button variant="link" onClick={() => history.push("/")}>首页</Button>
                     <Button variant="link" onClick={() => history.push("/login")}>登录</Button>
                     <Button variant="link" onClick={() => history.push("/register")}>注册</Button>
@@ -44,10 +54,16 @@ export default function Navbar() {
             )
         } else {
             return(
-                <ButtonGroup style={style}>
+                <ButtonGroup style={style.group}>
                     <Button variant="link" onClick={() => history.push("/")}>首页</Button>
                     <Button variant="link" onClick={() => history.push("/post")}>发表</Button>
-                    <Button variant="link" onClick={() => history.push("/user/" + username)}>{username}</Button>
+                    <Button variant="link" onClick={() => history.push("/space")}>动态</Button>
+                    <Avatar
+                        style={style.avatar}
+                        name={username}
+                        src={'data:image/png;base64,' + imageData}
+                        size="xs"
+                        onClick={() => window.location.href = "/user/" + username}/>
                     <Button variant="link" onClick={() => logout()}>退出</Button>
                 </ButtonGroup>
             )

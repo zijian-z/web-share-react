f4f0ca3057f78dd241d1b01bc4d19bdd39f53e1c
zijian-z <zijian-zhang@outlook.com>
2021-01-22 14:41:50 +0800

    用户信息维护界面

---
+++ src/component/profileUpdate.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {Input, Button, Text} from "@chakra-ui/react";
+import instance from "../config/axiosConfig";
+
+export default function ProfileUpdate(props) {
+    const [bio, setBio] = useState(props.bio);
+    const [email, setEmail] = useState(props.email);
+
+    const style = {
+        email: {
+            marginTop: '10px'
+        }
+    }
+    async function updateProfile() {
+        try {
+            const response = await instance.post('/profile/' + props.username, {
+                bio: bio,
+                email: email
+            });
+            window.location.href = "/user/" + props.username;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return(
+        <div>
+            <Text size="md">个人简介</Text>
+            <Input
+                value={bio}
+                onChange={(event) => setBio(event.target.value)}
+                variant="flushed"/>
+            <Text size="md" style={style.email}>邮箱</Text>
+            <Input
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                variant="flushed"/>
+            <Button
+                variant="link"
+                colorScheme="pink"
+                onClick={() => updateProfile()}>提交</Button>
+        </div>
+    )
+}

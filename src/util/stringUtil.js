c763d933529f640b0c5506bee2b3cd9447524527
zijian-z <zijian-zhang@outlook.com>
2021-01-22 14:39:40 +0800

    检查字符串和生成唯一的hash

---
+++ src/util/stringUtil.js
@@ -0,0 +1,13 @@
+import jsSHA from "jssha";
+import Identicon from "identicon.js";
+
+export function checkStringNotNull(s) {
+    return s !== null && s !== undefined && s !== '';
+}
+
+export function least15String(username) {
+    const shaObj = new jsSHA("SHA-224", "TEXT", { encoding: "UTF8" });
+    shaObj.update(username);
+    const hash = shaObj.getHash("HEX");
+    return new Identicon(hash, 420).toString()
+}

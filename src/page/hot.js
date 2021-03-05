be54c72e9faa6a8ec1fdced7e0682db7c03322e4
zijian-z <zijian-zhang@outlook.com>
2021-03-05 13:57:28 +0800

    新增排行榜页面

---
+++ src/page/hot.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import LinkBox from "../component/linkBox";
+import instance from "../config/axiosConfig";
+
+export default function Hot() {
+    const [linkArray, setLinkArray] = useState([]);
+
+    useEffect(() => {
+        async function getLinks() {
+            try {
+                const response = await instance.get('/links/hot/');
+                const linkArray = response.data;
+                setLinkArray(linkArray);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getLinks();
+    }, []);
+
+    return (
+        <div>
+            {linkArray.map((item) => <LinkBox key={item.id} link={item.uri} name={item.title} id={item.id}/>)}
+        </div>
+    );
+}
